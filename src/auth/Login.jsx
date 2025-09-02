0389802b95321b2231716d08752787e34ee51c8f
jaydeep4884 <sagathiyajaydeep66@gmail.com>
2025-09-02 11:17:35 +0530

    done setup started login page

---
+++ src/auth/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <>
+            <div className='text-white h-screen  flex justify-center items-center '>
+                <div className='p-5 border border-[#3c2e2e] rounded-lg w-5xl'>
+                    <div>
+                        <h1 className='text-[34px]'>Create Account</h1>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Login
